9ca2a850feff6a2675f6e82433a5f201b6a2bc2c
Thiago Lirio <thiagoglirio@hotmail.com>
2024-04-08 06:32:14 -0300

    Finalizado a página dos campeonatos

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -72,6 +72,54 @@ function criaInfoLateralEsquerdo(lateralEsquerdo) {
   return divInfoLateralEsquerdo;
 }
 
+function criaInfoVolante(volante) {
+  divInfoVolante = document.createElement("div");
+  imagemVolante = document.createElement("img");
+  nomeVolante = document.createElement("h4");
+
+  divInfoVolante.setAttribute("class", "column jogador");
+  imagemVolante.setAttribute("src", volante.image);
+  imagemVolante.innerHTML = volante.name;
+  nomeVolante.innerHTML = volante.name;
+
+  divInfoVolante.appendChild(imagemVolante);
+  divInfoVolante.appendChild(nomeVolante);
+
+  return divInfoVolante;
+}
+
+function criaInfoMeia(meia) {
+  divInfoMeia = document.createElement("div");
+  imagemMeia = document.createElement("img");
+  nomeMeia = document.createElement("h4");
+
+  divInfoMeia.setAttribute("class", "column jogador");
+  imagemMeia.setAttribute("src", meia.image);
+  imagemMeia.innerHTML = meia.name;
+  nomeMeia.innerHTML = meia.name;
+
+  divInfoMeia.appendChild(imagemMeia);
+  divInfoMeia.appendChild(nomeMeia);
+
+  return divInfoMeia;
+}
+
+function criaInfoAtacante(atacante) {
+  divInfoAtacante = document.createElement("div");
+  imagemAtacante = document.createElement("img");
+  nomeAtacante = document.createElement("h4");
+
+  divInfoAtacante.setAttribute("class", "column jogador");
+  imagemAtacante.setAttribute("src", atacante.image);
+  imagemAtacante.innerHTML = atacante.name;
+  nomeAtacante.innerHTML = atacante.name;
+
+  divInfoAtacante.appendChild(imagemAtacante);
+  divInfoAtacante.appendChild(nomeAtacante);
+
+  return divInfoAtacante;
+}
+
 function main() {
   let data = fazGet("https://flamengo-api.vercel.app/api/squad");
   let elenco = JSON.parse(data);
@@ -105,30 +153,20 @@ function main() {
     infoLateralEsquerdo.appendChild(divInfoLateralEsquerdo);
   });
 
-  // elenco.players.centerBacks.forEach((element) => {
-  //   let divInfoZagueiro = criaInfoZagueiro(element);
-  //   infoZagueiro.appendChild(divInfoZagueiro);
-  // });
+  elenco.players.midfielders.forEach((element) => {
+    let divInfoVolante = criaInfoVolante(element);
+    infoVolante.appendChild(divInfoVolante);
+  });
 
-  // elenco.players.centerBacks.forEach((element) => {
-  //   let divInfoZagueiro = criaInfoZagueiro(element);
-  //   infoZagueiro.appendChild(divInfoZagueiro);
-  // });
+  elenco.players.attackMidfielders.forEach((element) => {
+    let divInfoMeia = criaInfoMeia(element);
+    infoMeia.appendChild(divInfoMeia);
+  });
 
-  // elenco.players.centerBacks.forEach((element) => {
-  //   let divInfoZagueiro = criaInfoZagueiro(element);
-  //   infoZagueiro.appendChild(divInfoZagueiro);
-  // });
-
-  // elenco.players.centerBacks.forEach((element) => {
-  //   let divInfoZagueiro = criaInfoZagueiro(element);
-  //   infoZagueiro.appendChild(divInfoZagueiro);
-  // });
-
-  // elenco.players.centerBacks.forEach((element) => {
-  //   let divInfoZagueiro = criaInfoZagueiro(element);
-  //   infoZagueiro.appendChild(divInfoZagueiro);
-  // });
+  elenco.players.strikers.forEach((element) => {
+    let divInfoAtacante = criaInfoAtacante(element);
+    infoAtacante.appendChild(divInfoAtacante);
+  });
 }
 
 main();
